2b4e2e2f178a5bd51ef43826b9a558c96f00ac7e
Eldar Damari <eldar.damari@gmail.com>
2013-05-02 13:49:57 +0300

    updating task2c

--- lab4/task2c/my_cut.c
+++ lab4/task2c/my_cut.c
@@ -32,13 +32,15 @@ int main (int argc , char* argv[], char* envp[]) {
     char buf[BUF_SIZE];
     struct linux_dirent *d;
     int bpos;
-    char d_type, c;
+    char d_type;
+    char* buf_str[1];
     int flag = 0;
     int prefix = 0;
     int append = 0;
     int fd_app_source = 0;
+    int fd_run_read = 0;
+    int print = 0;
 
-    system_call(SYS_WRITE,STDOUT,"Flame2 Strikes Again! \n",23);
 
     if (argc == 3){ 
         if(strcmp("-p",argv[1]) == 0) {
@@ -48,10 +50,21 @@ int main (int argc , char* argv[], char* envp[]) {
                 append = 1;
             }
     }
-
-    fd = system_call(SYS_OPEN,".",0);
-    if (fd <0)
+    if (argc == 1){
+        print = 1;
+    }
+    if (print == 0 &&
+        append == 0 &&
+        prefix == 0)
         system_call(SYS_EXIT,0x55);
+
+
+    system_call(SYS_WRITE,STDOUT,"Flame2 Strikes Again! \n",23);
+    
+    fd = system_call(SYS_OPEN,".",0);
+    if (fd < 0)
+        system_call(SYS_EXIT,0x55);
+
 
     for ( ; ; ) {
         nread = system_call(SYS_GETDENTS, fd, buf, BUF_SIZE);
@@ -66,49 +79,63 @@ int main (int argc , char* argv[], char* envp[]) {
             d = (struct linux_dirent *) (buf + bpos);
             d_type = *(buf + bpos + d->d_reclen - 1);
 
+            /* Just to print directory */
+            if (print == 1) {
+
+                system_call(SYS_WRITE,STDOUT,d->d_name,strlen(d->d_name));
+                system_call(SYS_WRITE,STDOUT,"\n",1);
+            }
+
             if ( d_type == DT_REG ) { /* Checking if file is regular */
+                /*Prefix -p handle proccess*/
                 if (prefix == 1) {
 
                     if (strncmp(d->d_name, argv[2],strlen(argv[2])) == 0){
-
                         system_call(SYS_WRITE,STDOUT,d->d_name,strlen(d->d_name));
-                        system_call(SYS_WRITE,STDOUT,"fuck \n",6);
+                        system_call(SYS_WRITE,STDOUT,"\n",1);
                     } 
                 }
+                /*append -a  handle proccess*/
                 else if (append == 1) {
 
+                    /* Comparing strings with prefix from arguments */
                     if (strncmp(d->d_name, argv[2],strlen(argv[2])) == 0){
                         
+                        /* Printing file name on screen */
                         system_call(SYS_WRITE,STDOUT,d->d_name,strlen(d->d_name));
+                        system_call(SYS_WRITE,STDOUT,"\n",1);
 
-                        fd_app_source = 
-                            system_call(SYS_OPEN,"run",0,1);
+                        /* Opening for READ ONLY the exe to append file*/
+                        fd_app_source = system_call(SYS_OPEN,"run",0);
+                        if (fd_app_source < 0)
+                            system_call(SYS_EXIT,0x55);
 
-                        /*First Lets open the file matched with append */
-                        fd_app =  system_call(SYS_OPEN,d->d_name,1,1);
-                        if (fd_app > 0)
-                            system_call(SYS_WRITE,STDOUT,"file opened \n",13);
-                        system_call(SYS_LSEEK,fd_app,1,2);
+                        /*First Lets open the file matched with append premmisoin*/
+                        fd_app =  system_call(SYS_OPEN,d->d_name,1|1024,1);
+                        if (fd_app < 0)
+                            system_call(SYS_EXIT,0x55);
 
-                        //TODO!!!! need to append binary file "run" !! 
-                        while (system_call(SYS_READ,fd_app_source,c,1)){
-                            app_check = 
-                                system_call(SYS_WRITE,fd_app,c,strlen(c));
-                            if (app_check > 0)
-                                system_call(SYS_WRITE,STDOUT,"appended \n",13);
+
+                        /*Appending char by char*/
+                        while (system_call(SYS_READ,fd_app_source,buf_str,1) > 0)
+                        {
+                            app_check = system_call(SYS_WRITE,fd_app,buf_str,1);
+                            if (app_check < 0)
+                                system_call(SYS_EXIT,0x55);
                         }
-                        
+
+                        /*Setting the pointer of application to append to 
+                         * begining of file*/
                         system_call(SYS_LSEEK,fd_app_source,1,0);
 
-                        /*Writing into selected file - appending*/
-                        /* still need to append binary file */
-
+                        /*Closing file found in directory*/
                         system_call(SYS_CLOSE,fd_app);
-                        }
                     }
+                    /*Closing application to append*/
+                    system_call(SYS_CLOSE,fd_app_source);
                 }
-                bpos += d->d_reclen;
             }
+            bpos += d->d_reclen;
         }
-    system_call(SYS_CLOSE,fd_app_source);
+    }
 }
