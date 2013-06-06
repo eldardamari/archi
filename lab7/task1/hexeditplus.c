7b7828e0c1fc80e61949e86cec6c1f30194f7850
Eldar Damari <eldar.damari@gmail.com>
2013-06-06 12:48:40 +0300

    finished task1 -all working fine

--- lab7/task1/hexeditplus.c
+++ lab7/task1/hexeditplus.c
@@ -6,6 +6,8 @@
 /* forward declarations: */
 void memDisaply(char* filename); 
 void fileDisplay(char* filename);
+void fileModify(char* filename);
+void copyFromFile(char* filename);
 void quit(char* filename);
 
 struct fun_desc {
@@ -27,6 +29,8 @@ void memDisaply(char* filename) {
     int size,i;
     unsigned char* ptr; 
 
+    printf("Please enter <address> <length>\n");
+    
     fflush(stdin);
     fgets(sentence,100,stdin);
     fgets(sentence,100,stdin);
@@ -45,11 +49,13 @@ void memDisaply(char* filename) {
 }
 
 void fileDisplay(char* filename) {
-    
+   
     char sentence [100];
     unsigned int str[20];
     int fd,fdl,size,i;
     unsigned char c; 
+    
+    printf("Please enter <location> <length>\n");
 
 
     fd = open(filename,O_RDONLY);
@@ -83,6 +89,96 @@ void fileDisplay(char* filename) {
         close(fd);
 }
 
+void fileModify(char* filename) {
+    
+    char sentence [100];
+    unsigned int location[20];
+    unsigned int val[20];
+    int fd,fdl,size,i;
+    unsigned char c; 
+
+    printf("Please enter <location> <val>\n");
+    fd = open(filename,O_RDWR);
+    if (fd < 0) {
+        perror(filename);
+        exit(0);
+    }
+    
+    fflush(stdin);
+    fgets(sentence,100,stdin);
+    fgets(sentence,100,stdin);
+
+    sscanf(sentence,"%x %x",location,val);
+    
+    fdl = lseek(fd, *location ,SEEK_SET); 
+    if (fdl == -1) {
+        perror(filename);
+        exit(0);
+    }
+        write(fd,&val,1);
+    
+        /*reset values*/
+        size = 0;
+        i = 0;
+        close(fd);
+}
+
+void copyFromFile(char* filename) {
+    
+    char sentence [100];
+    int slocation;
+    int tlocation;
+    char localfilename[20];
+    int length;
+    int fd,fd2,fdl,i;
+    unsigned char c;
+
+    printf("Please enter <source-file> <s-location> <t-location> <length>\n");
+    
+    fflush(stdin);
+    fgets(sentence,100,stdin);
+    fgets(sentence,100,stdin);
+   
+    sscanf(sentence,"%s %x %x %d",
+            localfilename,
+            &slocation,
+            &tlocation,
+            &length);
+    /*open abc file*/
+    
+    fd = open(filename,O_RDWR);
+    if (fd < 0) {
+        perror(filename);
+        exit(0);
+    }
+
+    /*open local file from user*/
+    fd2 = open(localfilename,O_RDWR);
+    if (fd < 0) {
+        perror(localfilename);
+        exit(0);
+    }
+    
+    /*prepare file abc to be writen*/
+    if( lseek(fd, tlocation ,SEEK_SET) == -1 ){ 
+        perror(filename);
+        exit(0);
+    }
+    
+    /*reading from localfilename*/
+    if( lseek(fd2, slocation ,SEEK_SET) == -1 ){ 
+        perror(filename);
+        exit(0);
+    }
+    
+    for (i=0 ; i<length ; i++) {
+        read(fd2,&c,sizeof(c));
+        write(fd,&c,1);
+    }
+    
+        close(fd);
+        close(fd2);
+}
 void quit(char* filename){
     exit(0);
 }
@@ -92,6 +188,8 @@ int main(int argc, char **argv){
     struct fun_desc menu[] = { 
         { "Mem Display",  memDisaply  }, 
         { "File Display", fileDisplay },
+        { "File Modify", fileModify   },
+        { "Copy From File", copyFromFile},
         { "Quit",         quit        } }; 
 
     int arraySize,fNum;
@@ -111,15 +209,15 @@ int main(int argc, char **argv){
 
             while(1) 
             { 
-                printf("\n--- File: %s ,choose action:---\n",argv[1]); 
-                for(i=1 ; i <= 3 ; i++) 
+                printf("\n\n--- File: %s ,choose action:---\n",argv[1]); 
+                for(i=1 ; i <= 5 ; i++) 
                 { 
                     printf("%d) %s \n",i,menu[i-1].name); 
                 } 
 
                 /*Reading user choose */
                 scanf("%d",&fNum); 
-                if(fNum>3 || fNum<0){ 
+                if(fNum>6 || fNum<0){ 
                     return 0; 
                 } 
 
